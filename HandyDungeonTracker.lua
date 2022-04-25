5d4250861798aa40f5c9c8095f2d518f5f652058
Bronturra <bronturra@gmail.com>
2022-04-25 15:46:02 +0200

    added more functionality

--- HandyDungeonTracker.lua
+++ HandyDungeonTracker.lua
@@ -4,14 +4,24 @@ SlashCmdList["RL"] = function(msg)
 end
 
 -- Define Frame
-local f = CreateFrame("Frame")
+local moneyFrame = CreateFrame("Frame")
+local lootFrame = CreateFrame("Frame")
+
+
 -- Register Event when in Chat Money was looted
-f:RegisterEvent("CHAT_MSG_Money")
+moneyFrame:RegisterEvent("CHAT_MSG_Money")
+lootFrame:RegisterEvent("CHAT_MSG_Loot")
 
 local function logMoney()
     -- Simply for Test purpose, prints out if you looted any amount of money
     print("|cffff0050[HDT]|r" .. " - |cff40ea1aSomeone looted Money.|r")
 end
 
+local function logLoot()
+    -- Simply for Test purpose, prints out if you looted any item
+    print("|cffff0050[HDT]|r" .. " - |cff40ea1aSomeone looted an Item.|r")
+end
+
 -- Event trigger, executes function when the Event occurs
-f:SetScript("OnEvent", logMoney)
+moneyFrame:SetScript("OnEvent", logMoney)
+lootFrame:SetScript("OnEvent", logLoot)

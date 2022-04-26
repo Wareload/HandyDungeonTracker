54e3ca23b80935b94ddbf6eda46f4958e3f5ea5b
Wareload <wareload.developer@gmail.com>
2022-04-26 04:04:48 +0200

    implemented vendor item value and looted money value tracker

--- HandyDungeonTracker.lua
+++ HandyDungeonTracker.lua
@@ -1,27 +1,62 @@
+-- Define Slash Command
 SLASH_RL1 = "/RL"
-SlashCmdList["RL"] = function(msg)
+SlashCmdList["RL"] = function(_)
     ReloadUI();
 end
 
 -- Define Frame
-local moneyFrame = CreateFrame("Frame")
-local lootFrame = CreateFrame("Frame")
+local lootClearedFrame = CreateFrame("Frame")
+local lootOpenedFrame = CreateFrame("Frame")
+local lootClosedFrame = CreateFrame("Frame")
 
+--variables
+local money = nil
+local loot = {}
 
--- Register Event when in Chat Money was looted
-moneyFrame:RegisterEvent("CHAT_MSG_Money")
-lootFrame:RegisterEvent("CHAT_MSG_Loot")
+-- Register Event
+lootClearedFrame:RegisterEvent("LOOT_SLOT_CLEARED")
+lootOpenedFrame:RegisterEvent("LOOT_OPENED")
+lootClosedFrame:RegisterEvent("LOOT_CLOSED")
 
-local function logMoney()
-    -- Simply for Test purpose, prints out if you looted any amount of money
-    print("|cffff0050[HDT]|r" .. " - |cff40ea1aSomeone looted Money.|r")
+--called when open loot
+local function logOpenLoot()
+    for index = 1, GetNumLootItems() do
+        if LootSlotHasItem(index) then
+            local itemType = GetLootSlotType(index)
+            if itemType == 1 then
+                local _, _, lootQuantity = GetLootSlotInfo(index)
+                local itemLink = GetLootSlotLink(index)
+                local itemID = GetItemInfoFromHyperlink(itemLink);
+                local _, _, _, _, _, _, _, _, _, _, sellPrice = GetItemInfo(itemID)
+                loot[index] = (sellPrice * lootQuantity)
+            elseif itemType == 2 then
+                money = GetMoney()
+            end
+        end
+    end
 end
 
-local function logLoot()
-    -- Simply for Test purpose, prints out if you looted any item
-    print("|cffff0050[HDT]|r" .. " - |cff40ea1aSomeone looted an Item.|r")
+--called when taken an item from loot
+local function logClearedLoot(_, _, slot)
+    local value = loot[slot]
+    if value ~= nil then
+        print(value)--todo track in sessions
+        loot[slot] = nil
+    end
+
 end
+
+--called when closed loot
+local function logClosedLoot()
+    if money ~= nil then
+        print(GetMoney() - money)--todo track in sessions
+        money = nil
+    end
+    loot = {}
+end
+
 
 -- Event trigger, executes function when the Event occurs
-moneyFrame:SetScript("OnEvent", logMoney)
-lootFrame:SetScript("OnEvent", logLoot)
+lootClearedFrame:SetScript("OnEvent", logClearedLoot)
+lootOpenedFrame:SetScript("OnEvent", logOpenLoot)
+lootClosedFrame:SetScript("OnEvent", logClosedLoot)
